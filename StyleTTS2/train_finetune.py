046ed2000bd179e192e279eba016542b7ef0414c
JungSooKim <61766408+dmdm2002@users.noreply.github.com>
2025-01-14 17:35:56 +0900

    Add files via upload

--- StyleTTS2/train_finetune.py
+++ StyleTTS2/train_finetune.py
@@ -1,4 +1,6 @@
 # load packages
+import logging
+import os
 import random
 import yaml
 import time
@@ -29,7 +31,14 @@ from Modules.slmadv import SLMAdversarialLoss
 from Modules.diffusion.sampler import DiffusionSampler, ADPM2Sampler, KarrasSchedule
 
 from optimizers import build_optimizer
+import logging
+from logging import StreamHandler
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 # simple fix for dataparallel that allows access to class attributes
 
 
@@ -45,3 +54,59 @@ def main(config_path):
     config = yaml.safe_load(config_path)
 
     log_dir = config['log_dir']
+    if not osp.exists(log_dir): os.makedirs(log_dir, exist_ok=True)
+    shutil.copy(config_path, osp.join(log_dir, osp.basename(config_path)))
+    writer = SummaryWriter(f'{log_dir}/tensorboard')
+
+    # write logs
+    file_handler = logging.FileHandler(osp.join(log_dir, 'train.log'))
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter('%(levelname)s:%(asctime)s: %(message)s'))
+    logger.addHandler(file_handler)
+
+    batch_size = config.get('batch_size', 10)
+
+    epochs = config.get('epochs', 200)
+    save_freq = config.get('save_freq', 2)
+    log_interval = config.get('log_interval', 10)
+    saving_epoch = config.get('save_freq', 2)
+
+    data_params = config.get('data_params', None)
+    sr = config['preprocess_params'].get('sr', 24000)
+    train_path = data_params['train_data']
+    val_path = data_params['val_data']
+    root_path = data_params['root_path']
+    min_length = data_params['min_length']
+    OOD_data = data_params['OOD_data']
+
+    max_len = config.get('max_len', 200)
+
+    loss_params = Munch(config['loss_params'])
+    diff_epoch = loss_params.diff_epoch
+    joint_epoch = loss_params.joint_epoch
+
+    optimizer_params = Munch(config['optimizer_params'])
+
+    train_list, val_list = get_data_path_list(train_path, val_path)
+    device = config['device']
+
+    tr_dataloader = build_dataloader(train_list,
+                                     root_path,
+                                     OOD_data=OOD_data,
+                                     min_length=min_length,
+                                     batch_size=batch_size,
+                                     num_workers=0,
+                                     dataset_config={},
+                                     device=device)
+
+    val_dataloader = build_dataloader(val_list,
+                                      root_path,
+                                      OOD_data=OOD_data,
+                                      min_length=min_length,
+                                      batch_size=batch_size,
+                                      validation=True,
+                                      num_workers=0,
+                                      dataset_config={},
+                                      device=device)
+
+    
